544465fdccfb90e13ef94ae6f011985e5fc0926c
Hassani Assinaoui <hassani.assinaoui@gmail.com>
2023-03-13 18:53:05 +0100

    2nd commit first jsx lignes

--- kasa/src/index.jsx
+++ kasa/src/index.jsx
@@ -1,48 +1,34 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import Home from './Pages/Home/index'
-import Survey from './Pages/Survey/index'
-import Results from './Pages/Results/index'
-import Freelances from './Pages/Freelances/index'
-import Header from './Components/Header/index'
-import Error from './Components/Error/index'
-import GlobalStyle from './utils/style/globalStyle'
-import Footer from './Components/Footer'
-import { ThemeProvider, SurveyProvider } from './Utils/Context'
-import Profile from './Pages/Profile'
+import Header from './components/header/index'
+import Banner from './components/banner/index'
+import Home from './pages/home/index'
+import About from './pages/about/index'
+import Gallery from './components/gallery/index'
+import Error from './pages/404/index'
+import Footer from './components/footer/index'
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-      <ThemeProvider>
-        <SurveyProvider>
-          <GlobalStyle />
-          <Header />
-          <Switch>
-            <Route exact path="/">
-              <Home />
-            </Route>
-            <Route path="/survey/:questionNumber">
-              <Survey />
-            </Route>
-            <Route path="/results">
-              <Results />
-            </Route>
-            <Route path="/freelances">
-              <Freelances />
-            </Route>
-            <Route
-              path="/profile/:id"
-              render={(props) => <Profile {...props} />}
-            />
-            <Route path="*">
-              <Error />
-            </Route>
-          </Switch>
-          <Footer />
-        </SurveyProvider>
-      </ThemeProvider>
+      <Header />
+      <Banner />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/gallery">
+          <Gallery />
+        </Route>
+        <Route path="*">
+          <Error />
+        </Route>
+      </Switch>
+      <Footer />
     </Router>
   </React.StrictMode>,
   document.getElementById('root')
