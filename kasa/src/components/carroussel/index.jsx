3aa555e42313d38b80b2f4469a42071465fd9ab8
Hassani Assinaoui <hassani.assinaoui@gmail.com>
2023-03-16 15:24:18 +0100

    correctif carousel

--- kasa/src/components/carroussel/index.jsx
+++ kasa/src/components/carroussel/index.jsx
@@ -1,24 +1,24 @@
-import React, { useState } from 'react';
-import styled from 'styled-components';
+import React, { useState } from 'react'
+import styled from 'styled-components'
 import data from '../../data/logements.json'
-import leftArrow from '../../assets/left_carroussel.png';
-import rightArrow from '../../assets/right_carroussel.png';
+import leftArrow from '../../assets/left_carroussel.png'
+import rightArrow from '../../assets/right_carroussel.png'
 
 const CarouselContainer = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-between;
-  padding: 20px;
-  background-color: #f9f9f9;
-`;
+  background-color: gray;
+  border-radius: 25px;
+`
 
 const CarouselContent = styled.div`
   display: flex;
   flex-wrap: nowrap;
   overflow-x: scroll;
   scroll-behavior: smooth;
-  margin: 0 20px;
-  width: calc(100% - 160px);
+  height: 415px;
+  scroll-snap-type: x mandatory;
 
   &::-webkit-scrollbar {
     height: 8px;
@@ -37,35 +37,36 @@ const CarouselContent = styled.div`
   &::-webkit-scrollbar-thumb:hover {
     background-color: #aaa;
   }
-`;
+`
 
 const CarouselItem = styled.img`
   width: 100%;
-  max-width: 250px;
-  margin-right: 20px;
-`;
+  object-fit: cover;
+  scroll-snap-align: start;
+  border-radius: 25px;
+`
 
 const Arrow = styled.img`
-  height: 50px;
   cursor: pointer;
-`;
+`
 
 const Carousel = () => {
-  const [scrollPosition, setScrollPosition] = useState(0);
+  // eslint-disable-next-line no-unused-vars
+  const [scrollPosition, setScrollPosition] = useState(0)
 
   const handleScroll = (e) => {
-    setScrollPosition(e.target.scrollLeft);
-  };
+    setScrollPosition(e.target.scrollLeft)
+  }
 
   const handleLeftArrowClick = () => {
-    const carousel = document.getElementById('carousel');
-    carousel.scrollBy({ left: -250, behavior: 'smooth' });
-  };
+    const carousel = document.getElementById('carousel')
+    carousel.scrollBy({ left: -window.innerWidth, behavior: 'smooth' })
+  }
 
   const handleRightArrowClick = () => {
-    const carousel = document.getElementById('carousel');
-    carousel.scrollBy({ left: 250, behavior: 'smooth' });
-  };
+    const carousel = document.getElementById('carousel')
+    carousel.scrollBy({ left: window.innerWidth, behavior: 'smooth' })
+  }
 
   return (
     <CarouselContainer>
@@ -77,9 +78,13 @@ const Carousel = () => {
           ))
         )}
       </CarouselContent>
-      <Arrow src={rightArrow} alt="Right arrow" onClick={handleRightArrowClick} />
+      <Arrow
+        src={rightArrow}
+        alt="Right arrow"
+        onClick={handleRightArrowClick}
+      />
     </CarouselContainer>
-  );
-};
+  )
+}
 
-export default Carousel;
+export default Carousel
