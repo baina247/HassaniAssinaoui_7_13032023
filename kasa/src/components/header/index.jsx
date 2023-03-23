5cdea7078d95097b7e11457106438356e3d67eec
Hassani Assinaoui <hassani.assinaoui@gmail.com>
2023-03-23 14:12:50 +0100

    ajout de la nav et importation dans header

--- kasa/src/components/header/index.jsx
+++ kasa/src/components/header/index.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import styled from 'styled-components'
 import logo from '../../assets/logo.webp'
 import colors from '../../utils/style/colors'
+import Navigation from '../nav'
 
 const HeaderWrapper = styled.header`
   display: flex;
@@ -17,48 +18,11 @@ const Logo = styled.img`
   height: 50px;
 `
 
-const Nav = styled.nav`
-  display: flex;
-  justify-content: flex-end;
-  align-items: center;
-`
-
-const NavLink = styled.a`
-  position: relative;
-  margin-left: 30px;
-  font-size: 20px;
-  color: ${colors.secondary};
-  text-decoration: none;
-  &:before,
-  &:after {
-    content: '';
-    position: absolute;
-    width: 0;
-    height: 2px;
-    bottom: -2px;
-    transition: all 0.3s ease-out;
-    background-color: ${colors.secondary};
-  }
-  &:before {
-    left: 50%;
-  }
-  &:after {
-    right: 50%;
-  }
-  &:hover:before,
-  &:hover:after {
-    width: 50%;
-  }
-`
-
 function Header() {
   return (
     <HeaderWrapper>
       <Logo src={logo} alt="Image du header montrant le logo du site" />
-      <Nav>
-        <NavLink href="/">Accueil</NavLink>
-        <NavLink href="/about">A Propos</NavLink>
-      </Nav>
+      <Navigation />
     </HeaderWrapper>
   )
 }
---
+++ kasa/src/components/nav/index.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import styled from 'styled-components'
+import colors from '../../utils/style/colors'
+
+const Nav = styled.nav`
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+`
+
+const NavLink = styled.a`
+  position: relative;
+  margin-left: 30px;
+  font-size: 20px;
+  color: ${colors.secondary};
+  text-decoration: none;
+  &:before,
+  &:after {
+    content: '';
+    position: absolute;
+    width: 0;
+    height: 2px;
+    bottom: -2px;
+    transition: all 0.3s ease-out;
+    background-color: ${colors.secondary};
+  }
+  &:before {
+    left: 50%;
+  }
+  &:after {
+    right: 50%;
+  }
+  &:hover:before,
+  &:hover:after {
+    width: 50%;
+  }
+`
+const Navigation = () => {
+  return (
+    <Nav>
+      <NavLink href="/">Accueil</NavLink>
+      <NavLink href="/about">A Propos</NavLink>
+    </Nav>
+  )
+}
+
+export default Navigation
